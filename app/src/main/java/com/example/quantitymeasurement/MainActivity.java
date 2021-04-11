9ac75bc414a1586353079f3190526c2b43d4d890
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-04-11 16:16:50 +0530

    Added:Spinners to Different units

--- app/src/main/java/com/example/quantitymeasurement/MainActivity.java
+++ app/src/main/java/com/example/quantitymeasurement/MainActivity.java
@@ -3,23 +3,128 @@ package com.example.quantitymeasurement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Spinner Spinner1, fromUnitSpinner,toUnitSpinner;
+    String selectedClass, fromUnit ,toUnit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner1 = (Spinner) findViewById(R.id.Spinner1);
+        fromUnitSpinner = (Spinner) findViewById(R.id.fromUnitSpinner);
+        toUnitSpinner = (Spinner) findViewById(R.id.toUnitSpinner);
 
-        Spinner mySpinner = (Spinner) findViewById(R.id.spinner1);
+        Spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                String selectedClass = parent.getItemAtPosition(position).toString();
+                switch (selectedClass)
+                {
+                    case "Weight":
+                        // assigning div item list defined in XML to the div Spinner
+                        fromUnitSpinner.setAdapter(new ArrayAdapter<String>(MainActivity.this,
+                                android.R.layout.simple_spinner_dropdown_item,
+                                getResources().getStringArray(R.array.WeightUnits)));
+                        toUnitSpinner.setAdapter(new ArrayAdapter<String>(MainActivity.this,
+                                android.R.layout.simple_spinner_dropdown_item,
+                                getResources().getStringArray(R.array.WeightUnits)));
 
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Conversion));
-        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mySpinner.setAdapter(myAdapter);
+                        break;
+
+                    case "Length":
+                        fromUnitSpinner.setAdapter(new ArrayAdapter<String>(MainActivity.this,
+                                android.R.layout.simple_spinner_dropdown_item,
+                                getResources().getStringArray(R.array.LengthUnits)));
+
+                        toUnitSpinner.setAdapter(new ArrayAdapter<String>(MainActivity.this,
+                                android.R.layout.simple_spinner_dropdown_item,
+                                getResources().getStringArray(R.array.LengthUnits)));
+                        break;
+
+                    case "Volume":
+                        fromUnitSpinner.setAdapter(new ArrayAdapter<String>(MainActivity.this,
+                                android.R.layout.simple_spinner_dropdown_item,
+                                getResources().getStringArray(R.array.VolumeUnits)));
+
+                        toUnitSpinner.setAdapter(new ArrayAdapter<String>(MainActivity.this,
+                                android.R.layout.simple_spinner_dropdown_item,
+                                getResources().getStringArray(R.array.VolumeUnits)));
+                        break;
+
+                    case "Temperature":
+                        fromUnitSpinner.setAdapter(new ArrayAdapter<String>(MainActivity.this,
+                                android.R.layout.simple_spinner_dropdown_item,
+                                getResources().getStringArray(R.array.TemperatureUnits)));
+                        toUnitSpinner.setAdapter(new ArrayAdapter<String>(MainActivity.this,
+                                android.R.layout.simple_spinner_dropdown_item,
+                                getResources().getStringArray(R.array.TemperatureUnits)));
+                        break;
+                }
+
+                //set divSpinner Visibility to Visible
+                Spinner1.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+                // can leave this empty
+            }
+        });
+
+        fromUnitSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                fromUnit = parent.getItemAtPosition(position).toString();
+                /*
+                    Now that we have both values, lets create a Toast to
+                    show the values on screen
+                */
+                Toast.makeText(MainActivity.this, "\n Class: \t " + selectedClass +
+                        "\n Div: \t" + fromUnit, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+                // can leave this empty
+            }
+
+        });
+
+        toUnitSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                toUnit = parent.getItemAtPosition(position).toString();
+                /*
+                    Now that we have both values, lets create a Toast to
+                    show the values on screen
+                */
+                Toast.makeText(MainActivity.this, "\n Class: \t " + selectedClass +
+                        "\n Div: \t" + toUnit, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+                // can leave this empty
+            }
+
+        });
+
 
     }
 }
